e15ab0ef9bfab5f265555b26dc4491e5f4717b7b
joshirakesh165 <testing@gmail.com>
2024-02-29 23:32:31 +0530

    Code pushed from Api

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer style={{marginTop:'150px'}}>
+            <p>Copyright &#169; 2024 Rakesh joshi. All Rights Reserved. <br/></p>
+            <p style={{wordWrap:'break-word'}}>Design motivation for this portfolio is from https://www.youtube.com/@howtobecomeadeveloper youtube channel</p>
+        </footer>
+    )
+}
+
+export default Footer
